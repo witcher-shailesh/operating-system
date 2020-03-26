221981e072900a95df7c64ffa9b916fd8c736b28
witcher-shailesh <62455315+witcher-shailesh@users.noreply.github.com>
2020-03-26 21:13:20 +0530

    Update os_code.c

--- os_code.c
+++ os_code.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
+struct student
+{
+    int StudentId;
+    int FoodTakenTime;
+    int WaitingTime;
+    int TurnAroundTime;
+};
 int main()
 {
     struct student data[20];
     int n,i;
     char c='n';
-  
+     do
+	{
+    printf("Please enter the No. of Students wants to eat in mess? : ");
+    scanf("%d", &n);
+    accept(data, n);
+    scheduling(data, n);
+    waitingTime(data,n);
+    turnAroundTime(data,n);
+    display(data, n);
+    printf("Want to continue? press 'y' : ");
+    scanf("%s",&c);
+    }while(c=='y');
     return 0;
 } 
